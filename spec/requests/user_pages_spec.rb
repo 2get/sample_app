49c1e676596232ee8236445039702e859d5ac803
Mayoto Yoshida <2getjp@gmail.com>
2012-05-21 17:47:50 +0900

    テストファイルの最適化

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -3,11 +3,13 @@ require 'spec_helper'
   describe 'User pages' do
     subject { page }
 
+  let(:sign_up_text) { 'Sign up' }
+
     describe 'signup page' do
       before { visit signup_path }
 
-      it { should have_selector('h1', text: 'Sign up') }
-      it { should have_selector('title', text: full_title('Sign up')) }
+      it { should have_selector('h1', text: sign_up_text) }
+      it { should have_selector('title', text: full_title(sign_up_text)) }
     end
   end
 
