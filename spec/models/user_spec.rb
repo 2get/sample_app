33e34cce991c467a9b59a9ab50199c261538d2a5
Mayoto Yoshida <2getjp@gmail.com>
2012-05-25 16:00:05 +0900

    Finish user signup

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -25,7 +25,7 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
-  it { should_respond_to(:authenticate) }  # Listing 6.29
+  it { should respond_to(:authenticate) }  # Listing 6.29
 
   it { should be_valid }
 
@@ -73,6 +73,16 @@ foo@bar_baz.com foo@bar+baz.com]
     end
 
     it { should_not be_valid }
+  end
+
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      @user.reload.email.should == mixed_case_email.downcase
+    end
   end
 
   describe "when password is not present" do
