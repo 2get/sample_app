bc0d239ad6a9b31a22c15b1bb6efae099fcc5a78
Mayoto Yoshida <2getjp@gmail.com>
2012-05-28 16:28:35 +0900

    sslオフ

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -17,7 +17,7 @@ require 'spec_helper'
 
         describe 'after submission' do
           before { click_button submit }
-          
+
           it { should have_selector('title', text: 'Sign up') }
           it { should have_content('error') }
         end
@@ -52,10 +52,10 @@ require 'spec_helper'
       # Code to make a user variable
       let(:user) { FactoryGirl.create(:user) }
       before { visit user_path(user) }
-    
+
       it { should have_selector('h1', text: user.name) }
       it { should have_selector('title', text: user.name) }
-    end  
+    end
   end
 
 
