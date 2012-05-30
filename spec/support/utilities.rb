3f4db5ba3d319080abb60a57c6e6a8124d9597b5
Mayoto Yoshida <2getjp@gmail.com>
2012-05-30 14:28:28 +0900

    RSpec::Matchersをmatchers.rbにutilities.rbから分けた

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -15,8 +15,3 @@ def valid_signin(user)
   click_button 'Sign in'
 end
 
-RSpec::Matchers.define :have_error_message do |message|
-  match do |page|
-    page.should have_selector('div.alert.alert-error', text: message)
-  end
-end
