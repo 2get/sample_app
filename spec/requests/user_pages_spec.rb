2c18132bdab23d6f06035c1dfe93c8c5fc143cfd
Mayoto Yoshida <2getjp@gmail.com>
2012-05-31 12:51:59 +0900

    Chapter 9 Listing 9.16まで

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -60,8 +60,12 @@ require 'spec_helper'
 
     describe 'edit' do
       let(:user) { FactoryGirl.create(:user) }
-      before { visit edit_user_path(user) }
-
+      #before { visit edit_user_path(user) }
+      before do
+        sign_in user
+        visit edit_user_path(user)
+      end
+    
       describe 'page' do
         it { should have_selector('h1', text: 'Update your profile') }
         it { should have_selector('title', text: 'Edit user') }
