ead3f6b3c7fe919dc462bba65c4f2e2d1b316546
Mayoto Yoshida <2getjp@gmail.com>
2012-05-31 11:28:55 +0900

    Chapter 9 Listing 9.7まで

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -34,10 +34,12 @@ describe "Authentication" do
       #  fill_in 'Password', with: user.password
       #  click_button 'Sign in'
       #end
-      before { valid_signin(user) }
+      #before { valid_signin(user) }
+      before { sign_in user }
 
       it { should have_selector('title', text: user.name) }
       it { should have_link('Profile', href: user_path(user)) }
+      it { should have_link('Settings', href: edit_user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
 
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -15,3 +15,11 @@ def valid_signin(user)
   click_button 'Sign in'
 end
 
+def sign_in(user)
+  visit signin_path
+  fill_in 'Email', with: user.email
+  fill_in 'Password', with: user.password
+  click_button 'Sign in'
+  # Sign in when not using Capybara as well.
+  cookies[:remember_token] = user.remember_token
+end
