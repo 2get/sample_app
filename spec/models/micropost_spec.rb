541a5888cd6b8d6763848cb0e2880db73d7197ad
Mayoto Yoshida <2getjp@gmail.com>
2012-06-04 16:03:08 +0900

    Chapter 10 Listing 10.24まで

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -11,7 +11,7 @@ describe Micropost do
   it { should respond_to(:user_id) }
   it { should respond_to(:user) }
   its(:user) { should == user }
-  
+
   it { should be_valid }
 
   describe 'accessible attributes' do
@@ -24,6 +24,16 @@ describe Micropost do
 
   describe 'when user_id is not present' do
     before { @micropost.user_id = nil }
+    it { should_not be_valid }
+  end
+
+  describe 'with blank content' do
+    before { @micropost.content = ' ' }
+    it { should_not be_valid }
+  end
+
+  describe 'with content that is too long' do
+    before { @micropost.content = 'a' * 141 }
     it { should_not be_valid }
   end
 end
