c47063305601ff33bb6bb918b18b27b29b59b4b6
Mayoto Yoshida <2getjp@gmail.com>
2012-06-06 18:01:25 +0900

    Chapter 11 Listing 11.31まで

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -47,6 +47,7 @@ describe "Authentication" do
   end
 
   describe 'authorization' do
+
     describe 'for non-signed-in users' do
       let(:user) { FactoryGirl.create(:user) }
 
@@ -86,9 +87,19 @@ describe "Authentication" do
           before { visit users_path }
           it { should have_selector('title', text: 'Sign in') }
         end
+
+        describe 'visiting the following page' do
+          before { visit following_user_path(user) }
+          it { should have_selector('title', text: 'Sign in') }
+        end
+
+        describe 'visiting the followers page' do
+          before { visit followers_user_path(user) }
+          it { should have_selector('title', text: 'Sign in')}
+        end
       end
 
-      describe 'int the Microposts controller' do
+      describe 'in the Microposts controller' do
 
         describe 'submitting to the create action' do
           before { post microposts_path }
