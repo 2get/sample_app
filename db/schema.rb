4201946b2ef23e7a0b3053bb35a2b0c4cc5d21da
Mayoto Yoshida <2getjp@gmail.com>
2012-06-06 15:03:46 +0900

    relationshipsのmigrateファイル内スペルミス修正

--- db/schema.rb
+++ db/schema.rb
@@ -29,8 +29,8 @@ ActiveRecord::Schema.define(:version => 20120606021744) do
     t.datetime "updated_at",  :null => false
   end
 
-  add_index "relationships", ["followed_id", "followed_id"], :name => "index_relationships_on_followed_id_and_followed_id", :unique => true
   add_index "relationships", ["followed_id"], :name => "index_relationships_on_followed_id"
+  add_index "relationships", ["follower_id", "followed_id"], :name => "index_relationships_on_follower_id_and_followed_id", :unique => true
   add_index "relationships", ["follower_id"], :name => "index_relationships_on_follower_id"
 
   create_table "users", :force => true do |t|
