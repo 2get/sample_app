50cef24183ea2c4c9e74b44af53ceb1a63f08304
Mayoto Yoshida <2getjp@gmail.com>
2012-06-07 12:25:40 +0900

    Chapter 11 Listing 11.43まで

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -194,12 +194,43 @@ foo@bar_baz.com foo@bar+baz.com]
       subject { other_user }
       its(:followers) { should include(@user) }
     end
-    
+
     describe 'and unfollowing' do
       before { @user.unfollow!(other_user) }
 
       it { should_not be_following(other_user) }
       its(:followed_users) { should_not include(other_user) }
+    end
+  end
+
+  describe 'micropost associations' do
+    before { @user.save }
+    let!(:older_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
+    end
+    let!(:newer_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
+    end
+
+    describe 'status' do
+      let(:unfollowed_post) do
+        FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
+      end
+      let(:followed_user) { FactoryGirl.create(:user) }
+
+      before do
+        @user.follow!(followed_user)
+        3.times { followed_user.microposts.create!(content: 'Lorem ipsum') }
+      end
+
+      its(:feed) { should include(newer_micropost) }
+      its(:feed) { should include(older_micropost) }
+      its(:feed) { should_not include(unfollowed_post) }
+      its(:feed) do
+        followed_user.microposts.each do |micropost|
+          should include(micropost)
+        end
+      end
     end
   end
 end
