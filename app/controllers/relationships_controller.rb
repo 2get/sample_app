a3a082a79aa3d4f0b1533d1c01561432967b61cf
Mayoto Yoshida <2getjp@gmail.com>
2012-06-08 12:18:25 +0900

    Chapter 11 11.5 Exercises 2

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,23 +1,17 @@
 class RelationshipsController < ApplicationController
   before_filter :signed_in_user
 
+  respond_to :html, :js  # refactoring, Chapter 11 11.5 Exercises 2
+
   def create
     @user = User.find(params[:relationship][:followed_id])
     current_user.follow!(@user)
-    #redirect_to @user
-    respond_to do |format|
-      format.html { redirect_to @user }
-      format.js
-    end
+    respond_with @user
   end
 
   def destroy
     @user = Relationship.find(params[:id]).followed
     current_user.unfollow!(@user)
-    #redirect_to @user
-    respond_to do |format|
-      format.html { redirect_to @user }
-      format.js
-    end
+    responde_with @user
   end
 end
