535b9858b35b8e2d6076e9bdc2d629f4a7464f94
Mayoto Yoshida <2getjp@gmail.com>
2012-06-08 14:29:48 +0900

    clean

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -12,9 +12,7 @@ describe "StaticPages" do
   describe "Home page" do
     before { visit root_path }
 
-    #it { should have_selector('h1', :text => 'Sample App') }
     let(:heading) { 'Sample App' }
-    #it { should have_selector('title', :text => full_title('')) }
     let(:page_title) { '' }
 
     it_should_behave_like 'all static pages'
@@ -50,9 +48,7 @@ describe "StaticPages" do
 
   describe "Help page" do
     before { visit help_path }
-    #it { should have_selector('h1', :text => 'Help') }
     let(:heading) { 'Help' }
-    #it { should have_selector('title',  :text => full_title('Help')) }
     let(:page_title) { 'Help' }
 
     it_should_behave_like 'all static pages'
@@ -61,9 +57,7 @@ describe "StaticPages" do
   describe "About page" do
     before { visit about_path }
 
-    #it { should have_selector('h1', :text => 'About Us') }
     let(:heading) { 'About Us' }
-    #it { should have_selector('title',  :text => full_title('About Us')) }
     let(:page_title) { 'About Us' }
 
     it_should_behave_like 'all static pages'
@@ -71,9 +65,7 @@ describe "StaticPages" do
 
   describe "Contact page" do
     before { visit contact_path }
-    #it { should have_selector('h1', :text => 'Contact') }
     let(:heading) { 'Contact' }
-    #it { should have_selector('title', :text => full_title('Contact')) }
     let(:page_title) { 'Contact' }
 
     it_should_behave_like 'all static pages'
